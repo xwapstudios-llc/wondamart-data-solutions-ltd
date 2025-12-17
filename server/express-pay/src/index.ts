8540d396366f855b777afbba9981eb15e723df76
xwapstudios-llc <xwapstudios@gamil.com>
2025-12-17 21:09:11 +0000

    paystack as express and functions calls it

--- server/express-pay/src/index.ts
+++ server/express-pay/src/index.ts
@@ -10,58 +10,58 @@ import "@/express";
 
 const db = getFirestore();
 
-function handleTxAdded(tx: Tx) {
-    console.log("handleTxAdded", tx);
-    console.log("Will update status to processing after 10 seconds");
-    setTimeout(async () => {
-        const newTx = await TxFn.read(tx.id);
-        if (newTx.status == "pending") {
-            console.log("Tx status is still 'pending' after 10 seconds");
-            await TxFn.update_status_processing(tx.id);
-            console.log("Updated Tx status to processing");
-        } else {
-            console.log("Tx status is not 'pending' after 10 seconds");
-            console.log("Tx status > ", newTx.status);
-        }
-    }, 10 * 1000)
-}
+// function handleTxAdded(tx: Tx) {
+//     console.log("handleTxAdded", tx);
+//     console.log("Will update status to processing after 10 seconds");
+//     setTimeout(async () => {
+//         const newTx = await TxFn.read(tx.id);
+//         if (newTx.status == "pending") {
+//             console.log("Tx status is still 'pending' after 10 seconds");
+//             await TxFn.update_status_processing(tx.id);
+//             console.log("Updated Tx status to processing");
+//         } else {
+//             console.log("Tx status is not 'pending' after 10 seconds");
+//             console.log("Tx status > ", newTx.status);
+//         }
+//     }, 10 * 1000)
+// }
 
-function handleTxCanceled(tx: Tx) {
-    console.log("handleTxCanceled", tx);
-}
+// function handleTxCanceled(tx: Tx) {
+//     console.log("handleTxCanceled", tx);
+// }
+//
+// function handleTxFailed(tx: Tx) {
+//     console.log("handleTxFailed", tx);
+// }
+//
+// async function handleTxProcessing(tx: Tx) {
+// }
+//
+//
+// const fourHoursAgo = () => {
+//     return Timestamp.fromMillis(Timestamp.now().toMillis() - 4 * 60 * 60 * 1000)
+// }
 
-function handleTxFailed(tx: Tx) {
-    console.log("handleTxFailed", tx);
-}
-
-async function handleTxProcessing(tx: Tx) {
-}
-
-
-const fourHoursAgo = () => {
-    return Timestamp.fromMillis(Timestamp.now().toMillis() - 4 * 60 * 60 * 1000)
-}
-
-const doc_snapshots = (status: TxStatus) => {
-    return db.collection(collections.tx)
-        .where("status", "==", status)
-        .where("type", "==", "deposit")
-        .where("data.type", "==", "paystack")
-        .where("date", ">=", fourHoursAgo())
-}
+// const doc_snapshots = (status: TxStatus) => {
+//     return db.collection(collections.tx)
+//         .where("status", "==", status)
+//         .where("type", "==", "deposit")
+//         .where("data.type", "==", "paystack")
+//         .where("date", ">=", fourHoursAgo())
+// }
 
 // Watch on tx created
-doc_snapshots("pending")
-    .onSnapshot((snap) => {
-        snap.docChanges().forEach(async (change) => {
-            console.log("Status pending for ", change.doc.data())
-            if (change.type === "added") {
-                console.log("Into added on paystack pending");
-                const tx = change.doc.data() as Tx;
-                handleTxAdded(tx);
-            }
-        });
-    });
+// doc_snapshots("pending")
+//     .onSnapshot((snap) => {
+//         snap.docChanges().forEach(async (change) => {
+//             console.log("Status pending for ", change.doc.data())
+//             if (change.type === "added") {
+//                 console.log("Into added on paystack pending");
+//                 const tx = change.doc.data() as Tx;
+//                 handleTxAdded(tx);
+//             }
+//         });
+//     });
 
 // Watch on modified to failed
 // doc_snapshots("failed")
@@ -92,16 +92,16 @@ doc_snapshots("pending")
 
 
 // Watch on modified to processing
-doc_snapshots("processing")
-    .onSnapshot((snap) => {
-        snap.docChanges().forEach(async (change) => {
-            console.log("Tx status processing. Change type > ", change.type);
-            if (change.type === "added") {
-                const tx = change.doc.data() as Tx;
-                await handleTxProcessing(tx);
-            }
-        });
-    });
+// doc_snapshots("processing")
+//     .onSnapshot((snap) => {
+//         snap.docChanges().forEach(async (change) => {
+//             console.log("Tx status processing. Change type > ", change.type);
+//             if (change.type === "added") {
+//                 const tx = change.doc.data() as Tx;
+//                 await handleTxProcessing(tx);
+//             }
+//         });
+//     });
 
 
 console.log("Started Deposit Paystack");
