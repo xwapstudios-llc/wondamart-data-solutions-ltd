b3f9754c797ef3cb071b1decf1e9d37b5ffc3fa1
xwapstudios-llc <xwapstudios@gamil.com>
2025-12-23 13:20:31 +0000

    starts moved to ecosystem

--- server.ecosystem.config.js
+++ server.ecosystem.config.js
@@ -25,5 +25,30 @@ module.exports = {
             max_restarts: 12,
             restart_delay: 3000,
         },
+
+
+        {
+            name: "monitor-modem",
+            script: "pnpm",
+            args: "run start:monitor-modem",
+
+            // Hard requirements
+            cwd: `/home/${user}/wondamart-data-solutions-ltd`,
+
+            interpreter: "none",
+
+            env: {
+                WONDAMART_ROOT:
+                    `/home/${user}/wondamart-data-solutions-ltd`,
+                GOOGLE_APPLICATION_CREDENTIALS:
+                    `/home/${user}/.config/wondamart/wondamart-data-solutions-ltd-firebase-adminsdk-fbsvc-d418aa8130.json`,
+            },
+
+            // Ops hygiene
+            autorestart: true,
+            watch: false,
+            max_restarts: 12,
+            restart_delay: 3000,
+        },
     ],
 };
---
+++ vps.ecosystem.config.js
@@ -0,0 +1,29 @@
+const user = "xwapstudios";
+
+module.exports = {
+    apps: [
+        {
+            name: "express-pay",
+            script: "pnpm",
+            args: "run start:express-pay",
+
+            // Hard requirements
+            cwd: `/home/${user}/wondamart-data-solutions-ltd`,
+
+            interpreter: "none",
+
+            env: {
+                WONDAMART_ROOT:
+                    `/home/${user}/wondamart-data-solutions-ltd`,
+                GOOGLE_APPLICATION_CREDENTIALS:
+                    `/home/${user}/.config/wondamart/wondamart-data-solutions-ltd-firebase-adminsdk-fbsvc-d418aa8130.json`,
+            },
+
+            // Ops hygiene
+            autorestart: true,
+            watch: false,
+            max_restarts: 12,
+            restart_delay: 3000,
+        },
+    ],
+};
