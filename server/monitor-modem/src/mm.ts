c3f520113f418e28b3940fc6093da699dfb435d2
xwapstudios-llc <xwapstudios@gamil.com>
2025-12-23 17:36:02 +0000

    modem types

--- server/monitor-modem/src/mm.ts
+++ server/monitor-modem/src/mm.ts
@@ -53,6 +53,8 @@ export class ModemManagerClient {
         const modem = this.mmInterface.getInterface(
             "org.freedesktop.ModemManager1.Modem"
         ) as unknown as IModem;
+        console.log("Modem State:", modem.$properties.State);
+        console.log("Modem Signal Quality:", modem.$properties.SignalQuality);
         console.log("Modem Properties: ", JSON.stringify(modem.$properties, null, 2));
 
 
