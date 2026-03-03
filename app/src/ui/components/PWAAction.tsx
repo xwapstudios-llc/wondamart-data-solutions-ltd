97c87e48345e6262c917f4fdc2acabc70e60a82d
xwapstudios-llc <xwapstudios@gamil.com>
2026-03-03 22:55:31 +0000

    reboot pwa

--- app/src/ui/components/PWAAction.tsx
+++ app/src/ui/components/PWAAction.tsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import {cn} from "@/cn/lib/utils.ts";
 import {Button} from "@/cn/components/ui/button.tsx";
 import {useAppStore} from "@/lib/useAppStore.ts";
 import {registerSW} from "virtual:pwa-register";
+import {ArrowDown} from "lucide-react";
+import Code from "@/ui/components/typography/Code.tsx";
 
 type PWAActionProps = React.HTMLAttributes<HTMLDivElement>;
 const PWAAction: React.FC<PWAActionProps> = ({className, ...props}) => {
     const {deferAppInstallReady, setDeferAppInstallReady, setAppNeedUpdate, appNeedUpdate} = useAppStore();
+    const [clicked, setClicked] = useState(false);
+
     const updateApp = registerSW({
         onNeedRefresh: () => {
             setAppNeedUpdate(true)
         }
     });
+
     const installApp = () => {
         deferAppInstallReady.prompt();
         deferAppInstallReady.userChoice.then((choiceResult: { outcome: string; }) => {
@@ -20,6 +25,7 @@ const PWAAction: React.FC<PWAActionProps> = ({className, ...props}) => {
             }
             setDeferAppInstallReady(null);
         });
+        setClicked(false);
     }
 
     if (deferAppInstallReady || appNeedUpdate)
@@ -27,7 +33,21 @@ const PWAAction: React.FC<PWAActionProps> = ({className, ...props}) => {
         <div className={cn("flex items-center justify-center gap-4", className)} {...props}>
             {
                 deferAppInstallReady && (
-                    <Button onClick={installApp}>Install Wondamart Data Solutions</Button>
+                    <Button onClick={() => {
+                        setClicked(true);
+                        installApp()
+                    }}>
+                        {
+                            !clicked ? (
+                                <span>Install Wondamart</span>
+                            ) : (
+                                <span className={"flex gap-2"}>
+                                    Click <Code>ok</Code> or <Code>install</Code>
+                                    <ArrowDown className={"animate-bounce"} />
+                                </span>
+                            )
+                        }
+                    </Button>
                 )
             }
             {
